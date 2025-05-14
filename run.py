b9ddebe364a2c92447ea6add1ac7cda76fb65698
ikopmniko <hannapitman786@gmail.com>
2025-05-14 07:18:35 +0700

    Update run.py

--- run.py
+++ run.py
@@ -35,10 +35,10 @@ def read_csv_range(filename, start, end):
         rows = [row[0] for i, row in enumerate(csv.reader(f)) if start <= i < end]
     return rows
 
-start_row = 1450
-end_row = 1500
+start_row = 1500
+end_row = 1550
 
-email = "phoenix_martz@yahoo.com"
+email = "jarrod_hershberger@yahoo.com"
 
 
 
